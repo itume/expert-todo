9e8ddc46ba00f1402a45fbaf420b18a06a33ca3f
itume <jem90ham@gmail.com>
2018-02-16 11:17:25 +0900

    add validation to todo

--- app/models/todo.rb
+++ app/models/todo.rb
@@ -1,2 +1,5 @@
 class Todo < ApplicationRecord
+    validates title, presence: true, length:{ maximum: 30 }
+    validates description, length: { maximum: 100 }
+    validates done, presence: true
 end
