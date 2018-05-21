662a253ec8a219a0136c07733316cf598c230fed
itume <jem90ham@gmail.com>
2018-05-21 20:16:29 +0900

    add todo test and factory_bot

---
+++ spec/factories/todos.rb
@@ -0,0 +1,5 @@
+FactoryBot.define do
+  factory :todo do
+    title "卵買う"
+  end
+end
--- spec/models/todo_spec.rb
+++ spec/models/todo_spec.rb
@@ -1,5 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe Todo, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'done!' do
+    it 'タスクが完了状態になること' do
+      todo = create(:todo)
+      todo.done = false
+      todo.done!
+      expect(todo.done).to be true
+    end
+  end
+  describe 'undone!' do
+    it 'タスクが未完了状態になること' do
+      todo = create(:todo)
+      todo.done = true
+      todo.undone!
+      expect(todo.done).to be false
+    end
+  end
 end
