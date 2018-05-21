18840ab3f20e1de1ccaad29a38867008f9a1fa72
itume <jem90ham@gmail.com>
2018-05-21 20:51:05 +0900

    使わないテスト生成しない

--- config/application.rb
+++ config/application.rb
@@ -28,5 +28,13 @@ module ExpertTodo
     # Don't generate system test files.
     config.generators.system_tests = nil
     config.i18n.default_locale = :ja
+    config.generators do |g|
+      g.test_framework :rspec,
+            view_specs: false,
+            helper_specs: false,
+            controller_specs: false,
+            routing_specs: false,
+            request_specs: false
+    end
   end
 end
